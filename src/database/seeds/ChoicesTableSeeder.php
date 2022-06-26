95d7070d117e204a3ff3436df15ecf427d9a2b2f
あなたの名前 <>
2022-06-26 20:30:24 +0900

    東京都広島の最初の3問のダミーデータ追加

--- src/database/seeds/ChoicesTableSeeder.php
+++ src/database/seeds/ChoicesTableSeeder.php
@@ -13,10 +13,24 @@ class ChoicesTableSeeder extends Seeder
     public function run()
     {
         $param = [
-            ['prefectureId' => 1,
-            'questionId'=> 1,
-            'region' => 'たかなわ',
-            'valid' => 1]
+            ['prefectureId' => 1,'questionId'=> 1,'region' => 'たかなわ','valid' => 1],
+            ['prefectureId' => 1,'questionId'=> 1,'region' => 'こうわ','valid' => 0],
+            ['prefectureId' => 1,'questionId'=> 1,'region' => 'たかわ','valid' => 0],
+            ['prefectureId' => 1,'questionId'=> 2,'region' => 'かめいど','valid' => 1],
+            ['prefectureId' => 1,'questionId'=> 2,'region' => 'かめと','valid' => 0],
+            ['prefectureId' => 1,'questionId'=> 2,'region' => 'かめど','valid' => 0],
+            ['prefectureId' => 1,'questionId'=> 3,'region' => 'こうじまち','valid' => 1],
+            ['prefectureId' => 1,'questionId'=> 3,'region' => 'かゆまち','valid' => 0],
+            ['prefectureId' => 1,'questionId'=> 3,'region' => 'おかとまち','valid' => 0],
+            ['prefectureId' => 2,'questionId'=> 1,'region' => 'むかいなだ','valid' => 1],
+            ['prefectureId' => 2,'questionId'=> 1,'region' => 'むこうひら','valid' => 0],
+            ['prefectureId' => 2,'questionId'=> 1,'region' => 'むきひら','valid' => 0],
+            ['prefectureId' => 2,'questionId'=> 2,'region' => 'みつぎ','valid' => 1],
+            ['prefectureId' => 2,'questionId'=> 2,'region' => 'おしらべ','valid' => 0],
+            ['prefectureId' => 2,'questionId'=> 2,'region' => 'みよし','valid' => 0],
+            ['prefectureId' => 2,'questionId'=> 3,'region' => 'かなやま','valid' => 1],
+            ['prefectureId' => 2,'questionId'=> 3,'region' => 'ぎんざん','valid' => 0],
+            ['prefectureId' => 2,'questionId'=> 3,'region' => 'きやま','valid' => 0],
         ];
         DB::table('choices')->insert($param);
     }
