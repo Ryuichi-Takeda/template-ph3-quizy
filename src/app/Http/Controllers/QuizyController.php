4c1142b7167a7ebcf089ec76400a30b7b1673323
あなたの名前 <>
2022-06-30 21:30:54 +0900

    クエリビルダ使った

--- src/app/Http/Controllers/QuizyController.php
+++ src/app/Http/Controllers/QuizyController.php
@@ -10,15 +10,15 @@ class QuizyController extends Controller
 {
     public function quiz_list($prefecture_id)
     {
-        $choices_array=[];
-        for($i=1;$i<4;$i++){
-        $choices=
-            DB::select('select region from choices where prefectureId=? and questionId=?',[$prefecture_id,$i]);
-            array_push($choices_array,$choices);
-        }
+        // $choices_array=[];
+        // for($i=1;$i<4;$i++){
+        $choices_array=
+            DB::table('choices')->where('prefectureId',$prefecture_id)->where('questionId',1)->get();
+        //     array_push($choices_array,$choices);
+        // }
 
         $prefecture_name=
-            DB::select('select * from prefectures where id = ?',[$prefecture_id]);
+            DB::table('prefectures')->where('id',$prefecture_id)->get();
 
         return view ('quizy',['prefecture_id'=>$prefecture_id,'choices_array'=>$choices_array,'prefecture_name'=>$prefecture_name]);
     }
--- src/resources/views/quizy.blade.php
+++ src/resources/views/quizy.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
-<html lang="ja">a
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <title>quizy1</title>
     <link rel="stylesheet" href="{{ asset('css/quiz.css') }}">
 </head>
 <body>
+    {{-- <div>{{dd($choices_array)}}</div> --}}
     <h1>{{$prefecture_name[0]->prefecture}}の難読地名クイズ</h1>
     @foreach ($choices_array as $choices)
     <h2>{{$loop->index+1}}.この地名は何て読む？</h2>
-    @foreach ($choices as $choice)
-    <li>
+    @foreach ($choices_array as $choice)
+    <li id=`choice_{{$choice->questionId}}_{{$loop->index+1}}` data-valid={{$choice->valid}}>
         {{$choice->region}}
     </li>
     @endforeach
