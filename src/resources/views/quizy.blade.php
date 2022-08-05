74dc65bf10230f416e2df0a9fa40e96721c1dda1
あなたの名前 <>
2022-08-05 15:24:46 +0900

    jsとcss反映

--- src/resources/views/quizy.blade.php
+++ src/resources/views/quizy.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>quizy1</title>
-    <link rel="stylesheet" href="{{ asset('css/quiz.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/quizy.css') }}">
 </head>
 <body>
     {{-- <div>{{dd($choices_array)}}</div> --}}
