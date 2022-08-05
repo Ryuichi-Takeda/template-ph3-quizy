d615c5f0fe1a5a497be8bcfefa2c6b55e2ba9806
あなたの名前 <>
2022-08-05 18:46:17 +0900

    問題ページへの遷移リンクを作った

--- src/resources/views/quizy.blade.php
+++ src/resources/views/quizy.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="{{ asset('css/quizy.css') }}">
 </head>
 <body>
+    <a href="{{ asset('quizy/1') }}">東京</a>
     {{-- <div>{{dd($choices_array)}}</div> --}}
     <h1>{{$prefecture_name[0]->prefecture}}の難読地名クイズ</h1>
     @foreach ($choices_array as $choices)
