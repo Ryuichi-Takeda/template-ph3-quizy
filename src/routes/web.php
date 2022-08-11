fc1c1387761516a7497640c4726540f8c3626219
あなたの名前 <>
2022-08-12 01:21:20 +0900

    ミス修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,13 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('quizy', function () {
-    return view('quiz.quiz_list');
-});
-Route::get('quizy/{prefecture_id}', 'QuizyController@quiz_list');
+Route::get('quizy/{prefecture_id}', 'QuizyController@quiz_list')->name('quizy');
+Route::get('admin', 'AdminController@show')->name('admin');
+Route::get('admin/add', 'AdminController@add')->name('admin.add');
+Route::post('admin/add', 'AdminController@create');
+Route::get('admin/edit/{id}', 'AdminController@edit')->name('admin.edit');
+Route::post('admin/edit/{id}', 'AdminController@update');
+Route::get('admin/delete/{id}', 'AdminController@delete')->name('admin.delete');
+Route::post('admin/delete/{id}', 'AdminController@remove');
