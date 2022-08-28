273dca0ca51faeeb18b839f27d453be108769f9d
あなたの名前 <>
2022-08-28 14:48:22 +0900

    sorted with drag and drop

--- src/database/seeds/PrefecturesTableSeeder.php
+++ src/database/seeds/PrefecturesTableSeeder.php
@@ -14,8 +14,11 @@ class PrefecturesTableSeeder extends Seeder
     public function run()
     {
         $prefecturesParam = [
-            ['order_id'=>1,'prefecture' => '東京'],
-            ['order_id'=>2,'prefecture' => '広島'],
+            ['order_id'=>1,'prefecture' => '東京の難読地名クイズ'],
+            ['order_id'=>2,'prefecture' => '広島の難読地名クイズ'],
+            ['order_id'=>3,'prefecture' => '大阪の難読地名クイズ'],
+            ['order_id'=>4,'prefecture' => '京都の難読地名クイズ'],
+            ['order_id'=>5,'prefecture' => '奈良の難読地名クイズ'],
         ];
         DB::table('prefectures')->insert($prefecturesParam);
     }
--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -10,16 +10,41 @@
 
 <body>
     <a href="{{ route('admin.add') }}">追加</a>
-    <table>
-        @foreach ($prefectures as $prefecture)
-            <tr>
-                <td><a href="{{ route('admin.showQuestion',['id'=>$prefecture->id]) }}">{{ $prefecture->prefecture }}</a></td>
-                <td><a href="{{ route('admin.edit',['id'=>$prefecture->id]) }}">変更</a></td>
-                <td><a href="{{ route('admin.delete',['id'=>$prefecture->id]) }}">削除</a></td>
-            </tr>
-        @endforeach
-    </table>
+    <form action="" action="post">
+        <div class="sortable">
+            @csrf
+            @foreach ($prefectures as $prefecture)
+                <table>
+                    <tr>
+                        <td>
+                            <div>{{ $prefecture->order_id }}</div>
+                        </td>
+                        <td><a
+                                href="{{ route('admin.showQuestion', ['id' => $prefecture->id]) }}">{{ $prefecture->prefecture }}</a>
+                        </td>
+                        <td><a href="{{ route('admin.edit', ['id' => $prefecture->id]) }}">変更</a></td>
+                        <td><a href="{{ route('admin.delete', ['id' => $prefecture->id]) }}">削除</a></td>
+                    </tr>
+                </table>
+            @endforeach
+        </div>
+        <input type="hidden" id="list-ids" name="list-ids" />
+        <button id="submit">更新</button>
+    </form>
     </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+    <script>
+        $(function() {
+            $(".sortable").sortable();
+            $(".sortable").disableSelection();
+            $("#submit").click(function() {
+                var listIds = $(".sortable").sortable("toArray");
+                $("#list-ids").val(listIds);
+                $("form").submit();
+            });
+        });
+    </script>
 </body>
 
 </html>
