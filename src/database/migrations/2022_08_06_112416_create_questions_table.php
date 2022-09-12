b8ffcdb8e08fa77956a2e04775b582d5ad3efb26
あなたの名前 <>
2022-09-13 07:43:37 +0900

    changed the order_id from string into interger

--- src/database/migrations/2022_08_06_112416_create_questions_table.php
+++ src/database/migrations/2022_08_06_112416_create_questions_table.php
@@ -15,7 +15,7 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('order_id');
+            $table->interger('order_id');
             $table->integer('prefecture_id');
             $table->string('img');
             $table->timestamps();
